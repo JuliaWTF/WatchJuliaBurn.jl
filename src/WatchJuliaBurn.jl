dd841072a9beccc02ec7f1781a6f1923cf1d714c
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2021-04-21 18:45:09 +0200

    add more beauties

--- src/WatchJuliaBurn.jl
+++ src/WatchJuliaBurn.jl
@@ -8,6 +8,17 @@ const 🔥 = delete!
 const 🖨️ = print
 const 📡 = broadcast
 const 🧑🏻➡️🧑🏽 = tan
+const 🔢 = Matrix
+const ⚠ = warn
+const 🧺 = collect
+const 🪓 = split
+const ⬛  = nothing
+const 🔎 = findfirst
+const 🔍 = findlast
+const 🕵️ = findall
+const 🪓🪓 = axes
+const 🪟 = view️
+const ☝️ = show
 
 
 end
