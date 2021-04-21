a190dabfbf1390c30559752705d81d3a230bcf8c
Adrian Hill <adrian.hill@mailbox.org>
2021-04-21 17:59:47 +0200

    Add maps, dicts and boolean values

--- src/WatchJuliaBurn.jl
+++ src/WatchJuliaBurn.jl
@@ -1,7 +1,15 @@
 module WatchJuliaBurn
 
-export c╯°□°ↄ╯
+export c╯°□°ↄ╯, 🗺, 📖
+export ✅, 👍, 👌, 👎
 
+## Base
 const c╯°□°ↄ╯ = throw
+const 🗺 = map
+const 📖 = Dict
+const ✅ = true
+const 👍 = true
+const 👌 = true
+const 👎 = false
 
 end
