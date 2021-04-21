5b9e8352438f8b97e0ffb9b5f89c757405188c8c
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2021-04-21 17:01:43 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/WatchJuliaBurn.jl
@@ -0,0 +1,5 @@
+module WatchJuliaBurn
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using WatchJuliaBurn
+using Test
+
+@testset "WatchJuliaBurn.jl" begin
+    # Write your tests here.
+end
