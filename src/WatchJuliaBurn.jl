a81799386d9f69fc0c6f454c68611de43ecace65
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-04-22 19:17:45 +0200

    remove dependency on EmojiSymbols and use macro to create mapping (#14)
    * Remove EmojiSymbols and add new_emoji macro
    
    * Moved everything in a Dict
    
    * Remove double exports
    
    * Reexport @raw_str
    
    * Typo and add some docs

--- src/WatchJuliaBurn.jl
+++ src/WatchJuliaBurn.jl
@@ -1,91 +1,89 @@
 module WatchJuliaBurn
 
-export c╯°□°ↄ╯
-export 🗺, 📖, 📡
-export ✅, 👍, 👌, 👎
-export @🥩_str
-export 🖨️, ☝️
-export 🔥
-export ⬛
-export 🕵️
-export 💬🚨, 🚪🚶, 🗂, 📁, 🥇, 🔎🥇, 🔑, 🗝, 🎰, ⚰️, 🏃
-export 😻, ⬇️😻, ➡️😻, 😹, ⬇️😹, ➡️😹, 🐈, ⬇️🐈, ➡️🐈
-export 🛵🔧, ↔, 🔮, 🎠, 🔵
-export 🔢, 🧺
-export 🥧, 🍰, ㅠ
-export 🧑🏻➡️🧑🏽, 🗜️
+export @new_emoji
 
 using LinearAlgebra
-using EmojiSymbols
+const emoji_to_func = Dict{Any, Any}()
 
-## Base
-const c╯°□°ↄ╯ = throw
-const 🗺 = map
-const 📡 = broadcast
-const 📖 = Dict
-const ✅ = true
-const 👍 = true
-const 👌 = true
-const 👎 = false
+macro new_emoji(emoji, func)
+    emoji_to_func[emoji] = (func, "")
+    return esc(quote
+        export $emoji
+        const $emoji = $(func);
+    end)
+end
+
+macro new_emoji(emoji, func, julia_version)
+    julia_version = string(julia_version)
+    emoji_to_func[emoji] = (func, julia_version)
+    return esc(quote
+        if VERSION >= @v_str $(julia_version)
+            @eval export $(Symbol(emoji))
+            @eval const $(Symbol(emoji)) = $(func)
+        end
+    end)
+end
+
+## Contains mapping from functions to func_to_emojis
+## Each function (treated as a Symbol) maps to a Tuple of Symbols and Tuple{Symbol,Float64} for 
+## emojis needing a specific version
+const func_to_emojis = Dict(
+    ## Base
+    :throw => (:(c╯°□°ↄ╯),),
+    :map => (:(🗺),),
+    :broadcast => (:(📡),),
+    :Dict => (:(📖),),
+    :true => (:(✅), :(👍), :(👌),),
+    :false => (:👎,),
+    :delete! => (:(🔥),),
+    :print => (:(🖨️),),
+    :nothing => (:(⬛),),
+    :findall => (:(🕵️),), 
+    :show => (:(☝️),),
+    :peek => ((:(⛰️), 1.5),),
+    :chop => ((Symbol(Char(0x0001f333) * Char(0x0001fa93)), 1.2)), # 🌳🪓
+    :ArgumentError => (:(💬🚨),),
+    :join => (:(🚪🚶),),
+    :foldr => (:(🗂), :(📁),),
+    :first => (:(🥇),),
+    :findfirst => (:(🔎🥇),),
+    :keys => (:(🔑), :(🗝),),
+    :rand => (:(🎰),),
+    :kill => (:(⚰️),),
+    :run => (:(🏃),),
+    ## Arrays
+    :cat => (:(😻), :(😹), :(🐈),),
+    :vcat => (:(⬇️😻), :(⬇️😹), :(⬇️🐈),),
+    :hcat => (:(➡️😻), :(➡️😹), :(➡️🐈),),
+    :Matrix => (:(🔢),),
+    :collect => (:(🧺),),
+    :axes => ((Symbol(Char(0x0001fa93)^2), 1.2),), # 🪓🪓
+    :fill => (:(🚰),),
+    ## Math
+    :π => (:(🥧), :(🍰), :(ㅠ),),
+    :tan => (:(🧑🏻➡️🧑🏽),),
+    :log => ((Symbol(Char(0x0001fab5)), 1.5),), # 🪵
+    :clamp => (:(🗜️),),
+    :mod => (:(🛵🔧),),
+    :inv => (:(↔),),
+    :imag => (:(🔮),),
+    :round => (:(🎠), :(🔵),)
+)
+
+for func in keys(func_to_emojis)
+    for symbol_info in func_to_emojis[func]
+        if symbol_info isa Symbol 
+            @eval @new_emoji $(symbol_info) $(func)
+        elseif symbol_info isa Tuple
+            @eval @new_emoji $(symbol_info[1]) $(func) $(symbol_info[2])
+        end
+    end
+end
+
+## Additional features (does not pass with @new_emoji)
 @eval $(Symbol("@🥩_str")) = $(getfield(Main, Symbol("@raw_str")))
-const 🔥 = delete!
-const 🖨️ = print
-const ⬛  = nothing
-const 🕵️ = findall
-const ☝️ = show
-if VERSION >= v"1.5"
-    export ⛰️ 
-    const ⛰️ = peek
-end
-if VERSION >= v"1.2"
-    @eval export $(Symbol(Char(0x0001f333) * Char(0x0001fa93)))
-    @eval const $(Symbol(Char(0x0001f333) * Char(0x0001fa93))) = chop # 🌳🪓
-end
-const 💬🚨 = ArgumentError
-const 🚪🚶 = join
-const 🗂 = foldr
-const 📁 = foldr
-const 🥇 = first
-const 🔎🥇 = findfirst
-const 🔑 = keys
-const 🗝 = keys
-const 🎰 = rand
-const ⚰️ = kill
-const 🏃 = run
-
-
-## Arrays
-const 😻 = cat
-const ⬇️😻 = vcat
-const ➡️😻 = hcat
-const 😹 = cat
-const ⬇️😹 = vcat
-const ➡️😹 = hcat
-const 🐈 = cat
-const ⬇️🐈 = vcat
-const ➡️🐈 = hcat
-const 🔢 = Matrix
-const 🧺 = collect
-if VERSION >= v"1.2"
-    @eval export $(Symbol(Char(0x0001fa93) * Char(0x0001fa93)))
-    @eval const $(Symbol(Char(0x0001fa93) * Char(0x0001fa93))) = axes # 🪓🪓 
-end
-const 🚰 = fill
-
-## Math
-const 🥧 = π
-const 🍰 = π
-const ㅠ = π
-const 🧑🏻➡️🧑🏽 = tan
-if VERSION >= v"1.5"
-    @eval export $(Symbol(Char(0x0001fab5)))
-    @eval const $(Symbol(Char(0x0001fab5))) = log # 🪵
-end
-const 🗜️ = clamp
-const 🛵🔧 = mod
-const ↔ = inv
-const 🔮 = imag
-const 🎠 = round
-const 🔵 = round
+export @🥩_str
+func_to_emojis[:(raw"")] = (:(🥩""),)
+emoji_to_func[:(🥩"")] = (:(raw""), "")
 
 end
