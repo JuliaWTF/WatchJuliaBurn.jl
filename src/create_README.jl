1bddae80a054df038ddf3335977d9c87de7cd498
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-04-26 11:09:36 +0200

    Advertise emojify and PR in the README (#24)
    * Advertise emojify and PR in the README
    
    * Update create_README.jl
    
    Co-authored-by: Adrian Hill <adrian.hill@mailbox.org>

--- src/create_README.jl
+++ src/create_README.jl
@@ -23,8 +23,18 @@ md_ar = md(ar; latex=false)
 dots = "https://raw.githubusercontent.com/JuliaLang/julia/master/doc/src/assets/julia.ico"
 intro = """# ⌚<img src="$(dots)" height="26"/>🔥.jl
 
-WatchJuliaBurn aims at destroying the look of your code by adding emojis like :smile: and kaomojis like c╯°□°ↄ╯ instead of your favorites Julia functions.
-Don't hesitate to add your worst creations via PR.
+WatchJuliaBurn aims at destroying the look of your code by adding emojis like :smile: and kaomojis like c╯°□°ↄ╯ instead of your favorite Julia functions.
+
+## Add your own awfulness!
+
+Don't hesitate to add your worst creations via PR. All you need to do is to add the function and emoji to the `func_to_emojis` internal `Dict`. Don't touch the `README`!
+It will be automatically generated after your PR is merged. Also tests are optional since tests are for losers!
+
+## Emojify your code
+
+You can use the `emojify` function to recursively emojify all the files in a given path. `emojify` will replace all functions for which an alias is known
+by the corresponding emoji (a random one is picked every time if multiple options are possible).
+
 
 ## List of emojis
 
